13b2c40b9c91dd7a277e837c97c2e7e0994a554d
Cole Felix <89165165+VolsFelix@users.noreply.github.com>
2022-03-23 15:25:42 -0400

    Ballings example

--- Project2.py
+++ Project2.py
@@ -16,6 +16,8 @@ from sklearn.model_selection import train_test_split
 from tensorflow import feature_column
 from tensorflow.keras import layers
 
+
+
 PRICING= pd.read_csv('pricing.csv')
 PRICING.head()
 
@@ -23,52 +25,48 @@ PRICING.head()
 train, test = train_test_split(PRICING, test_size=0.2)
 train, val = train_test_split(train, test_size=0.2)
 
-# A utility method to create a tf.data dataset from a Pandas Dataframe
-# def df_to_dataset(PRICING, shuffle=True, batch_size=32):
-#   dataframe = dataframe.copy()
-#   labels = dataframe.pop('target')
-#   ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
-#   if shuffle:
-#     ds = ds.shuffle(buffer_size=len(dataframe))
-#   ds = ds.batch(batch_size)
-#   return ds
 
 ### First step is to encode the categorical variables: category and SKU
-category = feature_column.categorical_column_with_vocabulary_list(
-      'category', PRICING.category.unique())
-
-category_embedding = feature_column.embedding_column(category, dimension=1)
-
-sku = feature_column.categorical_column_with_vocabulary_list(
-      'sku', PRICING.sku.unique())
-
-sku_embedding = feature_column.embedding_column(sku, dimension=1)
 
 
-price= feature_column.numeric_column('price')
-duration = feature_column.numeric_column('duration')
 
-## creating feature columns
-feature_columns=[sku_embedding,price,duration, category_embedding]
+#######################
+#one categorical feature (one-hot) and one numeric feature
+#Embeddings
+
+#consider the following data where integers represent categories:
+x_cat = PRICING['category']
+x_num = PRICING['price'] #numeric
+#random y data
+y = PRICING['quantity']
 
 
-## creating feature dense layer
-feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+inputs_cat = tf.keras.layers.Input(shape=(1,),name = 'in_cat')
+embedding = tf.keras.layers.Embedding(input_dim=len(x_cat.unique()), output_dim=3, input_length=1,name = 'embedding')(inputs_cat)
+#Embedding shape is (None, 1, 3)
+#Need to flatten to make shape compatible with numeric input, which only has two dimensions: (None, 1)
+embedding_flat = tf.keras.layers.Flatten(name='flatten')(embedding) 
 
-# model = tf.keras.Sequential([
-#   feature_layer,
-#   layers.Dense(128, activation='relu'),
-#   layers.Dense(128, activation='relu'),
-#   layers.Dropout(.1),
-#   layers.Dense(1)
-# ])
+#input for the integer numbers
+inputs_num = tf.keras.layers.Input(shape=(1,),name = 'in_num')
 
-# model.compile(optimizer='adam',
-#               loss=tf.keras.losses.BinaryCrossentropy,
-#               metrics=['accuracy'])
+#input for embedding sku. Commented out for now Cause i Couldnt get it to fit in with everything else
+# inputs_sku = tf.keras.layers.Input(shape=(1,),name = 'in_sku')
+# embedding_sku = tf.keras.layers.Embedding(input_dim=len(PRICING['sku'].unique()), output_dim=3, input_length=1,name = 'embedding_sku')(inputs_sku)
+# embedding_flat_sku = tf.keras.layers.Flatten(name='flatten_sku')(embedding_sku)
 
-# model.fit(train,
-#           validation_data=val,
-#           epochs=10)
+#combinging the input layers
+inputs_concat = tf.keras.layers.Concatenate(name = 'concatenation')([embedding_flat, inputs_num])
+hidden = tf.keras.layers.Dense(2,name='hidden')(inputs_concat)
+outputs = tf.keras.layers.Dense(1, name = 'out')(hidden)
+
+
+model = tf.keras.Model(inputs = [inputs_cat,inputs_num], outputs = outputs)
+model.summary()
+model.compile(loss = 'mse', optimizer = tf.keras.optimizers.SGD(learning_rate = 0.001))
+model.fit(x=[x_cat,x_num],y=y, batch_size=100, epochs=1)
+
+
+
 
 
