c072d8cc69d82af1b2f2ea9867ebd91f6427cf60
livbedwell <89165368+livbedwell@users.noreply.github.com>
2022-03-30 19:45:15 -0400

    minor changes to nodes, spacing, and variable names

--- Project2_Function.py
+++ Project2_Function.py
@@ -1,11 +1,11 @@
-## Group 2 Deep Learning Project
+#### Group 2 Deep Learning Project
 # The goal of this assignment is to gather experience on the sensitivity of the algorithm to different
 # kinds of tuning parameters: batch size, number of hidden layers, number hidden neurons, hidden
 # activation functions (sigmoid, tanh, relu, leaky relu, prelu, elu),
 # optimizers (plain SGD, momentum, nesterov, adagrad, rmsprop, adam, learning rate scheduling)
 
-# The goal is to
-# predict quantity sold of a given product as accurately as possible by tuning the learning procedure
+# The goal is to predict quantity sold of a given product
+# as accurately as possible by tuning the learning procedure
 
 import pandas as pd
 import numpy as np
@@ -36,31 +36,30 @@ train, test = train_test_split(PRICING, test_size=0.2)
 train, val = train_test_split(train, test_size=0.2)
 
 
-### First step is to encode the categorical variables: category and SKU
-inputs_cat = tf.keras.layers.Input(shape=(1,),name = 'in_cats')
+#### First step is to encode the categorical variables: category and SKU
 # embedding category
-embedding_cat = tf.keras.layers.Embedding(input_dim=train['category'].nunique()+1, output_dim=3, input_length=1,name = 'embedding_cat')(inputs_cat)
+inputs_cat = tf.keras.layers.Input(shape=(1,),name = 'in_cats')
+embedding_cat = tf.keras.layers.Embedding(input_dim=PRICING['category'].nunique()+1, output_dim=16, input_length=1,name = 'embedding_cat')(inputs_cat)
 embedding_flat_cat = tf.keras.layers.Flatten(name='flatten')(embedding_cat)
 
 # embedding the sku
 inputs_sku = tf.keras.layers.Input(shape=(1,),name = 'in_sku')
-embedding_sku = tf.keras.layers.Embedding(input_dim=PRICING['sku'].nunique(), output_dim=3, input_length=1,name = 'embedding_sku')(inputs_sku)
+embedding_sku = tf.keras.layers.Embedding(input_dim=PRICING['sku'].nunique(), output_dim=100, input_length=1,name = 'embedding_sku')(inputs_sku)
 embedding_flat_sku = tf.keras.layers.Flatten(name='flatten2')(embedding_sku)
 
+#### Concatenation of all input layers
 # combining the categorical embedding layers
 cats_concat = tf.keras.layers.Concatenate(name = 'concatenation1')([embedding_flat_cat, embedding_flat_sku])
-
 #input for the quantity, price,order, and duration
 inputs_num = tf.keras.layers.Input(shape=(3,),name = 'in_num')
-
 #combinging the all input layers
 inputs_concat2 = tf.keras.layers.Concatenate(name = 'concatenation')([cats_concat, inputs_num])
 
-## Hidden Layers
-hidden1 = tf.keras.layers.Dense(2,activation='sigmoid',name='hidden')(inputs_concat2)
+#### Hidden Layers
+hidden = tf.keras.layers.Dense(2,activation='sigmoid')(inputs_concat2)
 
 #output layer
-outputs = tf.keras.layers.Dense(1, name = 'out')(hidden1)
+outputs = tf.keras.layers.Dense(1, name = 'out')(hidden)
 
 inputs=[inputs_cat,inputs_sku,inputs_num]
 
