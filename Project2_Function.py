b2fd1b4a78aac0f6a32a3a9a10ee4c3ac5e14094
livbedwell <89165368+livbedwell@users.noreply.github.com>
2022-04-07 17:04:14 -0400

    set batch size to 28

--- Project2_Function.py
+++ Project2_Function.py
@@ -214,7 +214,7 @@ dictionary = {'nodes_list': [[1000, 500, 250, 125, 75, 25], [5000, 2500, 1250, 7
               'initializer_name': ['glorot_uniform', 'glorot_normal', 'uniform', 'untruncated_normal', 'he_normal', 'he_uniform', 'he_avg_normal', 'he_avg_uniform'],
               'optimizer_name':["plain SGD","nesterov","RMSprop","Adam"],
               'epochs':[100],
-              'batch_size': [1, 28, 50],
+              'batch_size': [28],
               'clipnorm': [False]} # prioritize 28
 
 grid = expand_grid(dictionary)
